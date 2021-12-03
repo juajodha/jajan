59dfad5a3c81f076ef24c4c2635f44bf5ea2e081
juajodha <89195607+juajodha@users.noreply.github.com>
2021-12-03 23:16:18 +0700

    Update gen.js

--- gen.js
+++ gen.js
@@ -124,6 +124,7 @@ function nameGen(type){
           tag11=Math.floor(Math.random()*tanda11.length);
           tag14=Math.floor(Math.random()*tanda14.length);
 	  tag15=Math.floor(Math.random()*tanda15.length);
+	  tag16=Math.floor(Math.random()*tanda16.length);
           names=tanda16[tag16]+br2[rnd]+"\n"+tanda1[tag2]+tanda14[tag14]+tanda11[tag11]+tanda15[tag15]+"\n"+tanda3[tag3]+br1[rnd2]+tanda2[tag1];
         }
         else{
